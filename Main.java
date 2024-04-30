f9c02eee44574e675ea88b9813ca5ce5c106c2a7
Kevin Alejandro Leal Torres <lealtorreskevin@gmail.com>
2024-04-29 20:04:22 -0600

    proyecto comenzado

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Main {
+
+	public static void main(String[] args) {
+		var arrayList = new ArrayList<String>();
+
+		arrayList.add("kevin");
+		arrayList.add("alejandro");
+		arrayList.add("leal");
+		arrayList.add("torres");
+		arrayList.add("tecmilenio");
+		arrayList.add("cpp_lo_mejor");
+		arrayList.add("pythonzzzz");
+		arrayList.add("vaporeon");
+		arrayList.add("aifon");
+		arrayList.add("android");
+
+		System.out.println("Hello world\n");
+	}
+}
